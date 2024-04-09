717f964df11e3ec756360d380683a867460c9b29
feisalmombo <feisalmombo29@gmail.com>
2024-04-09 12:34:32 +0300

    create form to add students

---
+++ src/views/Students/Create.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-header">
+                <h4>Add Students</h4>
+            </div>
+            <div class="card-body">
+                <div class="mb-3">
+                    <label for="">Name</label>
+                    <input type="text" class="form-control" />
+                </div>
+
+                <div class="mb-3">
+                    <label for="">Course</label>
+                    <input type="text" class="form-control" />
+                </div>
+
+                <div class="mb-3">
+                    <label for="">Email</label>
+                    <input type="email" class="form-control" />
+                </div>
+
+                <div class="mb-3">
+                    <label for="">Phone</label>
+                    <input type="text" class="form-control" />
+                </div>
+
+                <div class="mb-3">
+                    <button type="button" class="btn btn-primary">Save</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  </template>
+  
