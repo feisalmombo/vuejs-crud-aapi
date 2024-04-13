252ea95798efd9771e626ce520510f164a8e63db
feisalmombo <feisalmombo29@gmail.com>
2024-04-13 14:12:49 +0300

    add route for edit student at index.js file

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import StudentView from '../views/Students/View.vue'
 import StudentCreate from '../views/Students/Create.vue'
+import StudentEdit from '../views/Students/Edit.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,12 @@ const router = createRouter({
       path: '/students/create',
       name: 'studentCreate',
       component: StudentCreate
+    },
+
+    {
+      path: '/students/:id/edit',
+      name: 'studentEdit',
+      component: StudentEdit
     }
   ]
 })
