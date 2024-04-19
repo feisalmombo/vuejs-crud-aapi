790753b3feb9e48b9c30418a5dd2a053dd0eaf95
feisalmombo <feisalmombo29@gmail.com>
2024-04-19 16:32:53 +0300

    WIP

--- src/views/Register/Register.vue
+++ src/views/Register/Register.vue
@@ -1,27 +1,20 @@
 <template>
 
  <div class="container">
-
   <div class="card" align="left">
     <div class="card-header">Register Form</div>
     <div class="card-body"> 
-    
       <form  @submit.prevent="saveData">
-        
         <label>First Name</label>
-        <input type="text" v-model="student.name" name="name" id="name" class ="form-control"/> 
+        <input type="text" v-model="student.name" name="name" id="name" class="form-control"/> 
 
-  
         <label>Email</label>
-        <input type="email" v-model="student.email" name="email" id="email" class ="form-control"/>
+        <input type="email" v-model="student.email" name="email" id="email" class="form-control"/>
 
         <label>Password</label>
-        <input type="password" v-model="student.password" name="password" id="password" class ="form-control"/> 
-
+        <input type="password" v-model="student.password" name="password" id="password" class="form-control"/> 
 
         <input type="submit" value="Save" class="btn btn-success"> 
-
-
       </form>
     </div>
 </div>
