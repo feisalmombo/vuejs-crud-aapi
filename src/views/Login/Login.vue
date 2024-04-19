365f21d92b792521ec3ae90ce48cc8453f5c5c4f
feisalmombo <feisalmombo29@gmail.com>
2024-04-19 17:21:39 +0300

    modify login.vue file

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,6 +1,64 @@
 <template>
-    <div class="about">
-      <h1>This is Login Page</h1>
+ <br>
+ <div class="container">
+  <div class="card" align="left">
+    <div class="card-header">Login Form</div>
+    <div class="card-body"> 
+      <form  @submit.prevent="LoginData">
+        <label>Email</label>
+        <input type="email" v-model="student.email" name="email" id="email" required="required" class="form-control" placeholder="Email"/>
+
+        <label>Password</label>
+        <input type="password" v-model="student.password" name="password" id="password" required="required" class="form-control" placeholder="Password"> 
+        <br>
+        <button type="submit" value="Save" class="btn btn-primary">Login</button>
+      </form>
     </div>
-  </template>
-  
+</div>
+ </div>
+
+</template>
+ 
+ <script>
+    import axios from 'axios'
+
+   export default {
+     name: 'Login',
+     data () {
+       return {
+         result: {},
+         student:{
+                    email: '',
+                    password: ''
+         }
+       }
+     },
+     created() { 
+     },
+     mounted() {
+           console.log("mounted() called.......");
+       },
+     methods: {
+            LoginData()
+            {
+             axios.post("http://127.0.0.1:8000/api/login", this.student)
+             .then(
+               ({data})=>{
+                console.log(data);
+                try {
+                if (data.status === true) {
+                      alert("Login Successfully"); 
+                      this.$router.push({ name: 'HelloWorld' })
+                      } else {
+                      alert("Login failed")
+                      }
+
+                      } catch (err) {
+                      alert("Error, please try again");
+                      }    
+               }
+             )
+            }
+       }
+   }
+   </script>
